2c2e2a3281f098e5d65f45c834ae9fb4621c288f
Pamilerin Coker <kehinde504@gmail.com>
2025-07-24 20:37:44 +0100

    json files

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,51 +1,31 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const serviceAccount = require(path.resolve(__dirname, 'serviceAccountKey.json'));
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://payvost-default-rtdb.firebaseio.com"
 });
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.api = void 0;
-const functions = __importStar(require("firebase-functions"));
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+
+const app = express();
+app.use(cors());
+
+// Basic route
 app.get('/', (_req, res) => {
-    res.send('API is working via Firebase Functions 🚀');
+  res.send('API is working via Firebase Functions 🚀');
 });
-// If needed, import backend logic like this:
-// import { userRouter } from '../backend/services/user';
+
+// Add more backend services if needed
+// const userRouter = require('../backend/services/user');
 // app.use('/user', userRouter);
+
 exports.api = functions.https.onRequest(app);
+
+// Optional standalone endpoint
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.send("Hello from Firebase Functions!");
+});
