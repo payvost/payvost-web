63ec368063efc63aa7ebc6038f93057093feea19
Pamilerin Coker <kehinde504@gmail.com>
2025-07-27 21:17:22 +0100

    pwa and cross origin

--- next.config.mjs
+++ next.config.mjs
@@ -36,4 +36,27 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+// next.config.mjs
+import nextPWA from 'next-pwa'
+import { join } from 'path'
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+})
+
+const pwaNextConfig = {
+  ...nextConfig,
+  reactStrictMode: true,
+  experimental: {
+    // This allows cross-origin requests to the Next.js dev server.
+    // This is required to allow devices on your local network (like your mobile phone)
+    // to access the dev server.
+    // The IP address should be the origin of the device making the request.
+    // You may need to adjust the port if your client is not running on port 3000.
+    allowedDevOrigins: ['http://192.168.237.4:3000'],
+  },
+}
+
+export default withPWA(pwaNextConfig);
