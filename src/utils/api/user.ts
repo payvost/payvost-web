1be28bac2639fe3d46cd057ee9736bd669b950c1
Pamilerin Coker <kehinde504@gmail.com>
2025-07-30 21:55:40 +0100

    add admin sdk

--- src/utils/api/user.ts
+++ src/utils/api/user.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/user';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/user';
 
 export const register = async (email: string, password: string, name?: string) => {
   return axios.post(`${API_URL}/register`, { email, password, name });
