7ed545d11c3808c911f04852628ced373e16da0a
Pamilerin Coker <kehinde504@gmail.com>
2025-09-30 19:25:01 +0100

    src update

---
+++ src/devDependencies.d.ts
@@ -0,0 +1,6 @@
+// This file is used to declare modules for packages that don't have TypeScript definitions
+// or to extend existing definitions.
+
+// For example, if you have a package named 'some-package' without types:
+// declare module 'some-package';
+declare module 'react-quill-new';
