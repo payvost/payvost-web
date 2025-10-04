a6e01221a8733af3ea2896bc8294ebb9ecaf1c3c
Pamilerin Coker <kehinde504@gmail.com>
2025-10-04 04:16:18 +0100

    src update

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const userAgent = request.headers.get('user-agent') || '';
-
-  // Regular expression to detect mobile and tablet devices
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-
-  // If a mobile user tries to access login or register, redirect them to the download page.
-  if (isMobile && (pathname === '/login' || pathname === '/register')) {
-    return NextResponse.redirect(new URL('/download', request.url));
-  }
-
   // Allow all other requests to proceed
   return NextResponse.next();
 }
