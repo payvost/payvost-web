37a101b653c6fd90f01f39f6d4e3ada8e6b35884
Pamilerin Coker <kehinde504@gmail.com>
2025-12-27 17:50:46 +0100

    fix: remove stale dist files from git tracking and update .gitignore
    - Add dist/ and compiled JS files to .gitignore
    - Remove all cached dist files so Render will recompile from fixed source
    - This prevents stale compiled code from being deployed
    - Fixes build errors caused by outdated dist/services/invoice/routes.js

--- backend/dist/services/payment/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.get('/', (req, res) => res.send('Payment Service Running'));
-app.listen(3003, () => console.log('Payment Service listening on port 3003'));
